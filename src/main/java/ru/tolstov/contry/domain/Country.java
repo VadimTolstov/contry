d2048812583922fc53aecc9f7fd7b6884d2d9a06
VadimTolstov <tolstov50@mail.ru>
2025-06-20 16:58:09 +0300

    Обработка ошибок валидация

--- src/main/java/ru/tolstov/contry/domain/Country.java
+++ src/main/java/ru/tolstov/contry/domain/Country.java
@@ -1,6 +1,5 @@
 package ru.tolstov.contry.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
